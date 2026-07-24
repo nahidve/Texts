a54d86e9f469965399271222dd489f6b86e2ce24
anushkapolleyVE <anushkapolley@virtualemployee.com>
2026-07-24 20:05:11 +0530

    Fix CORS issue for Vercel deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://AIGramX-frontend-swart.vercel.app"],
+  origin: ["http://localhost:5173", "https://AIGramX-frontend-swart.vercel.app", "https://texts-frontend-swart.vercel.app"],
   credentials: true,
 }));
 
--- backend/src/lib/socket.ts
+++ backend/src/lib/socket.ts
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://AIGramX-frontend-swart.vercel.app"],
+        origin: ["http://localhost:5173", "https://AIGramX-frontend-swart.vercel.app", "https://texts-frontend-swart.vercel.app"],
     },
 });
 
